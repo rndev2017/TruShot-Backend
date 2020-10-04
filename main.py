46333109cc94911d37e34c0f3a31d089ae13160f
Rohan Nagavardhan <rohans415@gmail.com>
2020-10-04 13:14:58 -0400

    Updates

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
-# FastAPI Packages
-from fastapi import FastAPI
-from src.storage import storage_router
+import uuid
+import creds
+import base64
+from fastapi import APIRouter, File, UploadFile, HTTPException
+from google.cloud.storage import Client, Blob
+from google.cloud.exceptions import GoogleCloudError
 
 app = FastAPI(title="truShot", version="0.0.1+1")
 
-app.include_router(storage_router)
+def init_cloud_storage(project_id: str) -> Client:
+    """ Intializes the Google Cloud Storage
+
+    Args:
+        project_id (str): Id of project in GCP
+
+    Returns:
+        storage_client (google.cloud.storage.Client): client to interface with Cloud Storage
+    """
+
+    # Generate storage client to access Storage buckets
+    storage_client = Client(project=project_id)
+
+    return storage_client
+
+
+@storage_router.post("/upload")
+async def upload_picture(file: bytes = File(None, media_type="image/jpeg")):
+    """
+    Uploads image from phone to server and saves it to bucket
+    Args:
+        file (bytes): the image taken from camera app encoded in bytes
+
+    Returns:
+        response (str): JSON Response with uuid of the file uploaded
+    """
+    try:
+        if file is None:
+            raise HTTPException(status_code=422, detail="Empty image sent")
+
+        else:
+            # Initializes the Storage client
+            storage_client = Client(project=creds.project_id)
+            bucket = storage_client.get_bucket(creds.bucket_id)
+
+            # Generates a unique identifier for storage
+            img_uuid = str(uuid.uuid4())[0:6]
+            blob = bucket.blob(img_uuid)
+
+            # Decodes the base64 encoded bytearry of incoming image
+            content = base64.b64decode(file)
+
+            # Takes base64 decoded image and converts to image/jpeg
+            blob.upload_from_string(data=content, content_type="image/jpeg")
+
+            return {"detail": img_uuid}
+    except GoogleCloudError as e:
+        raise HTTPException(detail=str(e), status_code=500)
+
+
